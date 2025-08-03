54ec74d8da75aaa75c61e552d6f5aec7a07414ee
miriam wangui <njerimiriam187@gmail.com>
2025-08-03 19:34:35 +0300

    added my simple calc assignment

---
+++ simpCalc.py
@@ -0,0 +1,4 @@
+firstNum =input("Enter the first number: ")
+secondNum =input("Enter the second number: ")
+result = int(firstNum )+ int(secondNum)
+print(str(firstNum )+ "+" +str( secondNum) + "=" + str(result))
